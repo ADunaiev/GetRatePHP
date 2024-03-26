81f4a3c65fbde72d8cb058d67fe59527757062b2
ADunaiev <111633434+ADunaiev@users.noreply.github.com>
2024-03-26 20:50:11 +0200

    DataWorker is cleaned

--- controllers/RequestsController.php
+++ controllers/RequestsController.php
@@ -78,21 +78,21 @@ class RequestsController extends ApiController {
             exit;
         } 
 
-        // запускаємо пошук прямих маршрутів
-        $routes = $this->find_simple_routes($start_point, $end_point);
-
         // формуємо масив для передачі результата
         $all_routes_and_rates = array();
         $dataWorker = new DataWorker();
-        
+
+        // запускаємо пошук прямих маршрутів
+        $routes = $dataWorker->find_simple_routes($start_point, $end_point);
+
         // заповнюємо результуючий масив прямими маршрутами
         foreach ( $routes as &$route) {
             $route['transport_type'] = 
-                $this->get_transport_type_name_by_trinity_code(
+                $dataWorker->get_transport_type_name_by_trinity_code(
                     $route['transport_type_trinity_code']
                 );
             $route['image'] = 
-                $this->get_transport_image_by_trinity_code(
+                $dataWorker->get_transport_image_by_trinity_code(
                     $route['transport_type_trinity_code']
                 );
 
@@ -144,19 +144,19 @@ class RequestsController extends ApiController {
         }
 
         // запускаємо пошук 2шагових маршрутів
-        $two_items_routes = $this->find_2_item_routes($start_point, $end_point);
+        $two_items_routes = $dataWorker->find_2_item_routes($start_point, $end_point);
 
         // додаємо в результуючий масив дані 2хшагових маршрутів
         foreach ( $two_items_routes as &$route) {
 
             // заповнюємо перший відрізок
             $route['image_first'] = 
-                $this->get_transport_image_by_trinity_code(
+                $dataWorker->get_transport_image_by_trinity_code(
                     $route['first_transport']
                 );
 
             $route['first_transport_name'] = 
-            $this->get_transport_type_name_by_trinity_code(
+            $dataWorker->get_transport_type_name_by_trinity_code(
                 $route['first_transport']
             );
 
@@ -196,12 +196,12 @@ class RequestsController extends ApiController {
 
             // заповнюємо другий відрізок
             $route['image_second'] = 
-            $this->get_transport_image_by_trinity_code(
+            $dataWorker->get_transport_image_by_trinity_code(
                 $route['second_transport']
             );
 
             $route['second_transport_name'] = 
-            $this->get_transport_type_name_by_trinity_code(
+            $dataWorker->get_transport_type_name_by_trinity_code(
                 $route['second_transport']
             );
 
@@ -250,19 +250,19 @@ class RequestsController extends ApiController {
         }
 
         // запускаємо пошук 3шагових маршрутів
-        $three_items_routes = $this->find_3_item_routes($start_point, $end_point);
+        $three_items_routes = $dataWorker->find_3_item_routes($start_point, $end_point);
 
 
         //  додаємо в результуючий масив дані 3хшагових маршрутів
         foreach ( $three_items_routes as &$route) {
             // заповнюємо перший відрізок
             $route['image_first'] = 
-                $this->get_transport_image_by_trinity_code(
+                $dataWorker->get_transport_image_by_trinity_code(
                     $route['1st_transport']
             );
 
             $route['first_transport_name'] = 
-            $this->get_transport_type_name_by_trinity_code(
+            $dataWorker->get_transport_type_name_by_trinity_code(
                 $route['1st_transport']
             );
 
@@ -302,12 +302,12 @@ class RequestsController extends ApiController {
 
             // заповнюємо другий відрізок
             $route['image_second'] = 
-            $this->get_transport_image_by_trinity_code(
+            $dataWorker->get_transport_image_by_trinity_code(
                 $route['2nd_transport']
             );
 
             $route['second_transport_name'] = 
-            $this->get_transport_type_name_by_trinity_code(
+            $dataWorker->get_transport_type_name_by_trinity_code(
                 $route['2nd_transport']
             );
 
@@ -347,12 +347,12 @@ class RequestsController extends ApiController {
 
             // заповнюємо третій відрізок
             $route['image_third'] = 
-            $this->get_transport_image_by_trinity_code(
+            $dataWorker->get_transport_image_by_trinity_code(
                 $route['3rd_transport']
             );
 
             $route['third_transport_name'] = 
-            $this->get_transport_type_name_by_trinity_code(
+            $dataWorker->get_transport_type_name_by_trinity_code(
                 $route['3rd_transport']
             );
 
@@ -406,7 +406,7 @@ class RequestsController extends ApiController {
 
         }
 
-
+        // робимо сортування
         if($sort_by == "price") {
             $total_sum = array_column($all_routes_and_rates, 'total_sum');
             array_multisort($total_sum, SORT_ASC, $all_routes_and_rates);
@@ -416,6 +416,7 @@ class RequestsController extends ApiController {
             array_multisort($total_transit_time, SORT_ASC, $all_routes_and_rates);
         }
 
+        // реалізуємо фільтрування маршрутів без ставок
         if ($withRates == "true") {
             $all_routes_and_rates = array_filter($all_routes_and_rates, function ($item) {
                 if ($item['with_rate'] == true ) {
@@ -444,233 +445,4 @@ class RequestsController extends ApiController {
 
         $this->end_with($result);
     }
-
-    // пошук прямих маршрутів
-    protected function find_simple_routes ($start_point_name, $end_point_name) {
-
-        $db = (new DataWorker())->connect_db_or_exit();
-
-        $sql = "SELECT transport_type_trinity_code, start_point_name, end_point_name
-                FROM routes
-                WHERE 
-                    start_point_name = ? AND 
-                    end_point_name = ?
-                GROUP BY  transport_type_trinity_code
-                ";
-
-        $result = null;
-
-        try {
-            $prep = $db->prepare($sql);
-            $prep->execute([
-                $start_point_name,
-                $end_point_name
-            ]);
-            $result = $prep->fetchAll();
-        }
-        catch(PDOException $ex) {
-            http_response_code(500);
-            echo "Connection error: " . $ex->getMessage();
-            exit;
-        } 
-
-        return $result;
-    }
-
-    // пошук маршрутів з 2-х відрізков
-    protected function find_2_item_routes ($start_point_name, $end_point_name) {
-        $db = (new DataWorker())->connect_db_or_exit();
-
-        $sql = "SELECT
-        r1.transport_type_trinity_code as first_transport, 
-        r1.start_point_name, 
-        r1.end_point_name as middle_point,
-        r2.transport_type_trinity_code as second_transport,
-        r2.end_point_name
-        from
-        routes as r1,
-        (SELECT
-                 r.transport_type_trinity_code, 
-                 r.start_point_name,
-                 r.end_point_name
-             FROM routes r
-             WHERE 	r.end_point_name = ? AND 
-                     r.start_point_name != r.end_point_name 
-            ) as r2
-        where 	r1.start_point_name = ? AND 
-                r1.start_point_name != r1.end_point_name AND
-                r1.end_point_name = r2.start_point_name 
-        group by 
-            first_transport,
-             middle_point,
-            second_transport;";           
-
-        $result = null;
-
-        try {
-            $prep = $db->prepare($sql);
-            $prep->execute([
-                $end_point_name,
-                $start_point_name
-            ]);
-            $result = $prep->fetchAll();
-        }
-        catch(PDOException $ex) {
-            http_response_code(500);
-            echo "Connection error: " . $ex->getMessage();
-            exit;
-        } 
-
-        return $result;
-    }
-
-    // пошук маршрутів з 3-х відрізков
-    protected function find_3_item_routes ($start_point_name, $end_point_name) {
-        $db = (new DataWorker())->connect_db_or_exit();
-
-        $sql = "select \n"
-                . "    r1.transport_type_trinity_code as 1st_transport, \n"
-                . "    r1.start_point_name, \n"         
-                . "    r1.end_point_name as middle_point1,\n"         
-                . "    r2.transport_type_trinity_code as 2nd_transport,\n"        
-                . "    r2.end_point_name as middle_point2,\n"  
-                . "    r3.transport_type_trinity_code as 3rd_transport,\n"        
-                . "    r3.end_point_name\n"         
-                . "from \n"          
-                . "	   routes as r1,\n"           
-                . "    transport_type as t,\n"           
-                . "    (SELECT \n"           
-                . "     	r.transport_type_trinity_code, \n"         
-                . "     	r.start_point_name,\n"           
-                . "     	r.end_point_name\n"          
-                . "     FROM routes r, transport_type t2\n"         
-                . "     WHERE 	\n"         
-                . "     		r.start_point_name != r.end_point_name AND\n"         
-                . "     		r.transport_type_trinity_code = t2.trinity_code\n"          
-                . "    ) as r2,\n"
-                . "    (SELECT \n"           
-                . "     	r.transport_type_trinity_code, \n"         
-                . "     	r.start_point_name,\n"           
-                . "     	r.end_point_name\n"          
-                . "     FROM routes r, transport_type t3\n"         
-                . "     WHERE 	r.end_point_name = ? AND \n"         
-                . "     		r.start_point_name != r.end_point_name AND\n"         
-                . "     		r.transport_type_trinity_code = t3.trinity_code\n"          
-                . "    ) as r3\n"            
-                . "\n"          
-                . "where 	r1.start_point_name = ? AND \n"            
-                . "		    r1.start_point_name != r1.end_point_name AND\n"           
-                . "         r1.end_point_name = r2.start_point_name AND\n"          
-                . "         r1.transport_type_trinity_code = t.trinity_code AND\n"
-                . "         r2.end_point_name = r3.start_point_name\n"             
-                . "group by \n"           
-                . "	    1st_transport,\n"           
-                . "     middle_point1,\n"          
-                . "     2nd_transport,\n"
-                . "     middle_point2,\n"          
-                . "     3rd_transport,\n"
-                . "     end_point_name;\n";
-                
-
-        $result = null;
-
-        try {
-            $prep = $db->prepare($sql);
-            $prep->execute([
-                $end_point_name,
-                $start_point_name
-            ]);
-            $result = $prep->fetchAll();
-        }
-        catch(PDOException $ex) {
-            http_response_code(500);
-            echo "Connection error: " . $ex->getMessage();
-            exit;
-        } 
-
-        return $result;
-    }
-
-    // пошук типу транспорту по коду трініті
-    protected function get_transport_type_name_by_trinity_code($ttype_id) {
-
-        $db = (new DataWorker())->connect_db_or_exit();
-
-        $sql = "SELECT `name` FROM transport_type
-        WHERE trinity_code = ?";
-
-        $result = "";
-
-        try {
-            $prep = $db->prepare($sql);
-            $prep->execute([
-                $ttype_id
-            ]);
-            $result = $prep->fetch()[0];
-        }
-        catch(PDOException $ex) {
-            http_response_code(500);
-            $result = "Error! City was not found.";
-            echo "Connection error: " . $ex->getMessage();
-            exit;
-        } 
-
-        return $result;
-    }
-
-    // пошук зображення типу транспорту по коду трініті
-    protected function get_transport_image_by_trinity_code($ttype_id) {
-
-        $db = (new DataWorker())->connect_db_or_exit();
-
-        $sql = "SELECT `image` FROM transport_type
-        WHERE trinity_code = ?";
-
-        $result = "";
-
-        try {
-            $prep = $db->prepare($sql);
-            $prep->execute([
-                $ttype_id
-            ]);
-            $result = $prep->fetch()[0];
-        }
-        catch(PDOException $ex) {
-            http_response_code(500);
-            $result = "Error! City was not found.";
-            echo "Connection error: " . $ex->getMessage();
-            exit;
-        } 
-
-        return $result;
-    }
-
-    // пошук зображення типу транспорту по назві транспорту
-    protected function get_transport_image_by_transport_name($ttype_name) {
-
-        $db = (new DataWorker())->connect_db_or_exit();
-
-        $sql = "SELECT `image` FROM transport_type
-        WHERE `name` = ?
-        LIMIT 1";
-
-        $result = "";
-
-        try {
-            $prep = $db->prepare($sql);
-            $prep->execute([
-                $ttype_name
-            ]);
-            $result = $prep->fetch();
-        }
-        catch(PDOException $ex) {
-            http_response_code(500);
-            $result = "Error! City was not found.";
-            echo "Connection error: " . $ex->getMessage();
-            exit;
-        } 
-
-        return $result;
-    }
-
 }
--- model/DataWorker.php
+++ model/DataWorker.php
@@ -2,6 +2,7 @@
 
 class DataWorker {
 
+    // підключення до бд
     public function connect_db_or_exit() {
         try {
             $settings = parse_ini_file( './config.ini', true);
@@ -22,6 +23,7 @@ class DataWorker {
         }
     }
 
+    // міста
     public function get_all_cities() {
 
         $db = $this->connect_db_or_exit();
@@ -46,6 +48,7 @@ class DataWorker {
         return $cities;
     }
 
+    // вантажи
     public function get_all_cargo() {
 
         $db = $this->connect_db_or_exit();
@@ -87,6 +90,7 @@ class DataWorker {
         return $cargo['name'];
     }   
 
+    // пошук ставок по маршруту з сортування по вартості
     public function get_route_rates($start_point_name, $end_point_name, $transport_type_trinity_code) {
 
         $db = $this->connect_db_or_exit();
@@ -121,6 +125,7 @@ class DataWorker {
         return $rates;
     }
 
+    // пошук ставок по транзитному часу
     public function get_route_rates_by_transit_time($start_point_name, $end_point_name, $transport_type_trinity_code) {
 
         $db = $this->connect_db_or_exit();
@@ -209,6 +214,34 @@ class DataWorker {
         return $result;
     }
 
+    // пошук зображення типу транспорту по назві транспорту
+    public function get_transport_image_by_transport_name($ttype_name) {
+
+        $db = (new DataWorker())->connect_db_or_exit();
+
+        $sql = "SELECT `image` FROM transport_type
+        WHERE `name` = ?
+        LIMIT 1";
+
+        $result = "";
+
+        try {
+            $prep = $db->prepare($sql);
+            $prep->execute([
+                $ttype_name
+            ]);
+            $result = $prep->fetch();
+        }
+        catch(PDOException $ex) {
+            http_response_code(500);
+            $result = "Error! City was not found.";
+            echo "Connection error: " . $ex->getMessage();
+            exit;
+        } 
+
+        return $result;
+    }
+
     // пошук прямих маршрутів
     public function find_simple_routes ($start_point_name, $end_point_name) {
 
@@ -240,6 +273,122 @@ class DataWorker {
             exit;
         } 
 
+        return $result;
+
     }    
     
+    // пошук маршрутів з 2-х відрізков
+    public function find_2_item_routes ($start_point_name, $end_point_name) {
+        $db = (new DataWorker())->connect_db_or_exit();
+
+        $sql = "SELECT
+        r1.transport_type_trinity_code as first_transport, 
+        r1.start_point_name, 
+        r1.end_point_name as middle_point,
+        r2.transport_type_trinity_code as second_transport,
+        r2.end_point_name
+        from
+        routes as r1,
+        (SELECT
+                 r.transport_type_trinity_code, 
+                 r.start_point_name,
+                 r.end_point_name
+             FROM routes r
+             WHERE 	r.end_point_name = ? AND 
+                     r.start_point_name != r.end_point_name 
+            ) as r2
+        where 	r1.start_point_name = ? AND 
+                r1.start_point_name != r1.end_point_name AND
+                r1.end_point_name = r2.start_point_name 
+        group by 
+            first_transport,
+             middle_point,
+            second_transport;";           
+
+        $result = null;
+
+        try {
+            $prep = $db->prepare($sql);
+            $prep->execute([
+                $end_point_name,
+                $start_point_name
+            ]);
+            $result = $prep->fetchAll();
+        }
+        catch(PDOException $ex) {
+            http_response_code(500);
+            echo "Connection error: " . $ex->getMessage();
+            exit;
+        } 
+
+        return $result;
+    }
+
+    // пошук маршрутів з 3-х відрізков
+    public function find_3_item_routes ($start_point_name, $end_point_name) {
+        $db = (new DataWorker())->connect_db_or_exit();
+
+        $sql = "select 
+                        r1.transport_type_trinity_code as 1st_transport,
+                        r1.start_point_name,          
+                        r1.end_point_name as middle_point1,
+                        r2.transport_type_trinity_code as 2nd_transport,        
+                        r2.end_point_name as middle_point2, 
+                        r3.transport_type_trinity_code as 3rd_transport,       
+                        r3.end_point_name
+                from routes as r1           
+                left join transport_type as t
+                on r1.transport_type_trinity_code = t.trinity_code
+                inner join (SELECT
+                        r.id,
+                        r.transport_type_trinity_code,          
+                        r.start_point_name,           
+                        r.end_point_name          
+                        FROM routes r, transport_type t2         
+                        WHERE 	         
+                        r.start_point_name != r.end_point_name AND         
+                        r.transport_type_trinity_code = t2.trinity_code          
+                        ) as r2
+                on r1.end_point_name = r2.start_point_name
+                inner join  (SELECT 
+                            r.id,
+                            r.transport_type_trinity_code,          
+                            r.start_point_name,           
+                            r.end_point_name          
+                            FROM routes r, transport_type t3         
+                            WHERE 	r.end_point_name = ? AND          
+                            r.start_point_name != r.end_point_name AND         
+                            r.transport_type_trinity_code = t3.trinity_code         
+                            ) as r3  
+                ON  r2.end_point_name = r3.start_point_name
+                where 	r1.start_point_name = ? AND             
+                r1.start_point_name != r1.end_point_name
+                group by            
+                            1st_transport,           
+                            middle_point1,          
+                            2nd_transport,
+                            middle_point2,          
+                            3rd_transport,
+                            end_point_name;";
+                
+
+        $result = null;
+
+        try {
+            $prep = $db->prepare($sql);
+            $prep->execute([
+                $end_point_name,
+                $start_point_name
+            ]);
+            $result = $prep->fetchAll();
+        }
+        catch(PDOException $ex) {
+            http_response_code(500);
+            echo "Connection error: " . $ex->getMessage();
+            exit;
+        } 
+
+        return $result;
+    }
+
 }
